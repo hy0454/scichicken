c313dbc9813aa1332f20ec50542a2e7d5930ef81
hy0454 <38546003+hy0454@users.noreply.github.com>
2018-04-27 09:14:56 +0800

    Create update.sh
    已经安装文件

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "欢迎使用十六哥的一键搭建"
+echo "即将搭建的是战术雷达4.27版本"
+echo "准备开始安装"
+read -p "回车后开始安装"
+echo "请输入你的内网ip" 
+read -p "内网ip： " ip
+cp /root/winnerpubg/restart.sh /root/restart.sh
+chmod +x restart.sh
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+
+echo "ss代理搭建成功，请记住连接信息"
+read -p "记住了吗？任意键继续！" 
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
+source ~/.bash_profile
+nvm install v9.8.0
+nvm alias default v9.8.0
+yum -y install gcc-c++
+yum -y install flex
+yum -y install bison
+wget http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+tar -zxvf libpcap-1.8.1.tar.gz
+cd libpcap-1.8.1
+./configure
+make
+make install
+
+git clone https://github.com/hy0454/scichicken.git
+cd scichicken/
+npm i
+npm i -g pino
+npm install -g forever
+forever start index.js sniff eth0 $ip | pino
+
+echo "搭建完成"
